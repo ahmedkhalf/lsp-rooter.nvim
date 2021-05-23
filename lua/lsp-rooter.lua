7e468b43b8d3542438c1b3197192213230f84d26
ahmedkhalf <ahmedkhalf567@gmail.com>
2021-05-23 15:50:47 +0400

    Finally fix multiple lsp clients bug

--- lua/lsp-rooter.lua
+++ lua/lsp-rooter.lua
@@ -17,12 +17,12 @@ end
 local get_lsp_client = function()
   -- Get lsp client for current buffer
   local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-  local clients = vim.lsp.get_active_clients()
+  local clients = vim.lsp.buf_get_clients()
   if next(clients) == nil then
     return nil
   end
 
-  for _, client in ipairs(clients) do
+  for _, client in pairs(clients) do
     local filetypes = client.config.filetypes
     if filetypes and vim.fn.index(filetypes,buf_ft) ~= -1 then
       return client
